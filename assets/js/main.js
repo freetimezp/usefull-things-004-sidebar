7f687488cbbf13d168b07fbef07966257fe212e2
freetimezp <kraftukrnet@ukr.net>
2025-02-09 16:03:17 +0200

    06. usefull-things-004-sidebar. Active sidebar link on click.

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,9 +12,18 @@ const showSidebar = (toggleId, sidebarId) => {
 
 showSidebar('header-toggle', 'sidebar');
 
-
-
 /*=============== LINK ACTIVE ===============*/
+const sidebarLink = document.querySelectorAll('.sidebar__list a');
+
+function linkColor() {
+    sidebarLink.forEach(el => el.classList.remove('active-link'));
+
+    this.classList.add('active-link');
+}
+
+if (sidebarLink) {
+    sidebarLink.forEach(link => link.addEventListener('click', linkColor));
+}
 
 
 /*=============== DARK LIGHT THEME ===============*/
