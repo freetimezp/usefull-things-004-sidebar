89052b7b0e13cc8c1946075ed710d7cd4f66bf91
freetimezp <kraftukrnet@ukr.net>
2025-02-09 16:44:52 +0200

    07. usefull-things-004-sidebar. Dark theme toggle.

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,4 +27,32 @@ if (sidebarLink) {
 
 
 /*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'ri-sun-fill';
 
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => {
+    return document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+};
+
+const getCurrentIcon = () => {
+    return themeButton.classList.contains(iconTheme) ? 'ri-moon-clear-fill' : 'ri-sun-fill';
+};
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'ri-moon-clear-fill' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    console.log(selectedTheme);
+
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+});
