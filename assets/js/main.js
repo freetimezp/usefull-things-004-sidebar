3292b1db25e6c86d1adcc6b2d3732103ddf4cf49
freetimezp <kraftukrnet@ukr.net>
2025-02-09 15:51:28 +0200

    05. usefull-things-004-sidebar. Sidebar toggle.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,21 @@
 /*=============== SHOW SIDEBAR ===============*/
+const showSidebar = (toggleId, sidebarId) => {
+    const toggle = document.getElementById(toggleId);
+    const sidebar = document.getElementById(sidebarId);
+
+    if (toggle && sidebar) {
+        toggle.addEventListener('click', () => {
+            sidebar.classList.toggle('show-sidebar');
+        });
+    }
+};
+
+showSidebar('header-toggle', 'sidebar');
+
 
 
 /*=============== LINK ACTIVE ===============*/
 
 
-/*=============== DARK LIGHT THEME ===============*/ 
+/*=============== DARK LIGHT THEME ===============*/
 
